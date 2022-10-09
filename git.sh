7c6174203e2b1856a8d5bd9782f8e517556fae29
Tripura kant <tripurakant@gmail.com>
2022-10-09 14:12:45 +0000

    new

---
+++ git.sh
@@ -0,0 +1,6 @@
+  git config --global user.email "tripurakant@gmail.com"
+  git config --global user.name "Tripura kant"
+cd /home/tripurakan97ine/DevOps-Pro
+git add .
+git commit -m "new"
+git push
